417bc14e0b8217aad56cb18ff81f29e4a2ae9e21
smellyalater <acopleman@gmail.com>
2015-11-03 00:10:31 -0500

    done.  Can't figure out #2

--- javascript-challenge-2.js
+++ javascript-challenge-2.js
@@ -22,7 +22,36 @@ function testScores(name, score) {
 var ralph = new testScores('Ralph', 90);
 var zack = new testScores('Zack', 80);
 var tony = new testScores('Tony', 100);
-//I'm assuming there is a way to automate/loop through the console log output, I don't know how to do it and the way it stands satisfies all requirements.
+//I'm assuming there is a way to automate/loop through the console log output, I don't know how to do it and the way it stands satisfies all requirements so I'm just going to leave it.
 console.log(ralph.name + " got a " + ralph.score + " on the test.");
 console.log(zack.name + " got a " + zack.score + " on the test.");
 console.log(tony.name + " got a " + tony.score + " on the test.");
+
+
+
+/*
+2. Create a form on a Web page where a user can enter the year they were born. When the user submits the form, it should display text on the page that says:
+
+This person is x years old
+ 
+
+Note: To do this one, read up on the jQuery .submit() handler https://api.jquery.com/submit/. Pay close attention to the event.preventDefault() line in the examples. What happens if you leave that out?
+*/
+
+
+//<AMC>No idea how to get this to work. So frusturating.
+
+//On submit, the form captures the birth year. 
+$('#form-submit').submit(function (event) {
+var year = $('#entered-year').val();//is this .value() or .val()?
+event.preventDefault();
+});
+//Then a function subtracts 2015 - birth year.
+ var age = 2015 - year;
+//Then we say that the person is xx years old. 
+$('#put-age-in-here').text("This person is " + age + " years old.");
+
+
+
+
+
